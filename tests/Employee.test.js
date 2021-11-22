b01cf227d95984a6367bd0cf448acbc913b0779a
Estefany Silfa <esilfa3@outlook.com>
2021-11-22 18:50:56 -0500

    start adding test pass

---
+++ tests/Employee.test.js
@@ -0,0 +1,17 @@
+import Employee from '../lib/Employee';
+const e = new Employee();
+
+describe('Employee', () => {
+    it('Should return Employee instance', () => {
+
+        expect(typeof (e).toBe('object'))
+
+    })
+});
+it('Should retun set name', () => {
+    expect(e.name).toBe(name);
+
+});
+it
+
+
